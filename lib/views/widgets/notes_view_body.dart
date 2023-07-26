8bc1c56f7081a01b0bcf7edd348d53d48c50b87c
Kamikazee112 <S.mohanad2003@gmail.com>
2023-07-26 22:28:20 +0300

    edit note done

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 import 'notes_list_view.dart';
 
@@ -12,7 +12,10 @@ class NotesViewBody extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height: 50),
-          CustomAppBar(),
+          CustomAppBar(
+            title: "Notes",
+            icon: Icons.search,
+          ),
           Expanded(child: NotesListView())
         ],
       ),
